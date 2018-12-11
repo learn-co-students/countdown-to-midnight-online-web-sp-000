7f6b7f755ec207598091f2bdd0ff285055d41bd0
Aglae Verduzco <aglae.cv@gmail.com>
2018-12-11 01:31:11 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(x)
+  while x > 0
+  puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(num)
+  five_sec = sleep 5
+  while num > 0
+    puts "#{num} SECOND(S)!"
+      num -= five_sec
+    end
+    return "HAPPY NEW YEAR!"
 end
