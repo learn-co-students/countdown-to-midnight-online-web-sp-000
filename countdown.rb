8f325e23748e9c7b1c9f0a2ba908f2e4ff1b8dcc
Yingqi Chen <chanwk18@hotmail.com>
2019-03-22 20:23:39 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(count)
+  until count==0
+    puts "#{count} SECOND(S)!"
+    count-=1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(count)
+  until count==0
+    puts "#{count} SECOND(S)!"
+    count-=1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
 end
