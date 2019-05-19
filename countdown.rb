6aaa042570d0b7fc63063daf91b6d881ce1f2002
kevin lang <langkevina@gmail.com>
2019-05-19 22:17:07 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(cnt)
+  # cnt = 10
+  if cnt == 12
+    return "HAPPY NEW YEAR!"
+  end
+  while cnt > 0
+    puts "#{cnt} SECOND(S)!"
+    cnt -= 1
+  end
+end
+
+def countdown_with_sleep(cnt)
+  if cnt == 12
+    return "HAPPY NEW YEAR!"
+  end
+  while cnt > 0
+    sleep 1
+    puts "#{cnt} SECOND(S)!"
+    cnt -= 1
+  end
 end
