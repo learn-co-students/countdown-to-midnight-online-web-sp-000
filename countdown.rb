f6f9bf49bb354a2ad8fc3c0e94e366224b7f312a
Connor Cowan <ccowan003@comcast.net>
2019-05-31 01:08:27 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,16 @@
-def countdown(number)
-  number = 10
-  while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
   end
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number)
-  number = 10
-  while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
+def countdown_with_sleep(seconds_to_midnight)
+  seconds_to_midnight = 10
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
     sleep(1.0)
   end
   return "HAPPY NEW YEAR!"
