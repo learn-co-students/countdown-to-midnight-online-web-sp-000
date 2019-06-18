a57e25709959f4042eba84979e81ab4dc81e5b43
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-18 18:58:24 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
+def countdown(count)
+     until count == 0
+     puts "#{count} SECOND(S)!"
+     count -= 1
+     end
+   "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(count)
+  until count == 0
+  puts "#{count} SECOND(S)!"
+  count -= 1
+  sleep(1)
+  end
 end
