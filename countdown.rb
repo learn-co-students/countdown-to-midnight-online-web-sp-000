644f4f8af4979971b61f485c25aeb368a0d952fc
Christopher Dent <christopherdent01@gmail.com>
2019-07-19 22:31:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,34 @@
 #write your code here
 
-def countdown
-end
+
+def countdown(x)
+
+  while x > 0
+    puts "#{x} SECOND(S)!"
+      x = x - 1 
+  end
+
+  if x == 0 
+    return "HAPPY NEW YEAR!"
+  end 
+
+end 
+
+#write second code here
+
+
+def countdown_with_sleep(x)
+  
+  while x > 0
+    puts "#{x} SECOND(S)!"; sleep 5 
+      x = x - 1 
+  end
+
+  if x == 0 
+    p "HAPPY NEW YEAR!"
+  end 
+
+end 
+  
+ 
+
