b6ce874bae04fb7d20a4fef453f873012ebfad27
David Strauch <dmaster18@gmail.com>
2019-07-23 14:52:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
-
-def countdown
+integer_argument = 20
+def countdown(integer_argument)
+  while (integer_argument > 0 )
+    puts "#{integer_argument} SECOND(S)!"
+    integer_argument-=1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer_argument)
+   while (integer_argument > 0 )
+    puts "#{integer_argument} SECOND(S)!"
+    integer_argument-=1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
+
+countdown(20)
+countdown_with_sleep(20)
