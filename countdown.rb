8fc42113198deca5ef45f8c4f0c3de21954bed2f
favorable-device-5158 <blee2125@gmail.com>
2019-08-14 18:39:37 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+seconds = 12
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -=1
 
-def countdown
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -=1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
