bedb2b32de7989f6cc926d1333d6710631835ffa
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-12 16:33:21 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
-#write your code here
+num_of_sec = 10 
 
-def countdown
+def countdown (num_of_sec)
+  while num_of_sec > 0 
+    puts "#{num_of_sec} SECOND(S)!"
+    num_of_sec -= 1 
+  end
+ return "HAPPY NEW YEAR!"
 end
+countdown (num_of_sec)
+
+
+sleep 5
+
+num_of_sec = 10 
+
+def countdown_with_sleep (num_of_sec)
+  while num_of_sec > 0 
+    puts "#{num_of_sec} SECOND(S)!"
+    sleep 1
+    num_of_sec -= 1 
+  end
+ return "HAPPY NEW YEAR!"
+end
+countdown_with_sleep (num_of_sec)
