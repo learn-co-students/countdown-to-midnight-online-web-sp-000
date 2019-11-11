d5a00822649419c8d8da9dad2e5e6a9213df3220
Erica Little <eteney90@gmail.com>
2019-11-11 23:47:35 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
-#write your code here
+def countdown(input)
+  while input > 0 do
+    puts "#{input} SECOND(S)!"
+    input -= 1
+  end
+  if input == 0
+    return "HAPPY NEW YEAR!"
+end
+end
 
-def countdown
+def countdown_with_sleep(input)
+  sleep(5)
 end
