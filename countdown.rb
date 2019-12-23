7c62f0c75214cec2daff2fe6a8f3f231fe2f8b17
Doak Covington <doakcovington@protonmail.com>
2019-12-23 00:14:37 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown (int)
+  seconds =  int
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  seconds = int
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    sleep(1)
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
