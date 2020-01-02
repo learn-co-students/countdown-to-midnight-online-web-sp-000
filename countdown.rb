e7acf74d0aa840c0472ba193b4e32432dd377507
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-02 04:46:22 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown(num)
+  while num > 0 do 
+    puts "#{num} SECOND(S)!"
+    num -= 1 
+  end 
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(time)
+  sleep(time)
+end 
+
