9b5d34ef13aadfc8399b66a3f0c548da7579758b
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-23 20:46:17 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
-
-def countdown
+def countdown(integer)
+  while integer > 0 
+  puts "#{integer} SECOND(S)!" 
+  integer -= 1 
+  sleep 2
+  while integer == 0 
+  return "HAPPY NEW YEAR!"
 end
