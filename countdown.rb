851fc5559c2d9cbfe2d3d7789c10de8a65b5acf5
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-06 20:58:56 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  timer = integer
+  while timer > 0 do
+    puts "#{timer} SECOND(S)!"
+      timer -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(integer)
+  timer = integer
+  while timer > 0 do
+    puts "#{timer} SECOND(S)!"
+      timer -= 1
+      sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
