7e61650c266fa097e6132968e27c1ff1f57ee380
Matt Im <matthew.seiim@gmail.com>
2020-02-22 19:52:57 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(i)
+  while i > 0
+    puts "#{i} SECOND(S)!"
+  i -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(i)
+  sleep(i)
 end
