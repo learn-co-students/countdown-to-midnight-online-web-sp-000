2f179a6f1055c87daee977ab98f454855b4feb99
Nick Lanese <nlanese21@gmail.com>
2020-04-30 19:01:22 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(currentVal)
+  while currentVal > 0
+    puts "#{currentVal} SECOND(S)!"
+    currentVal -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(currentVal)
+  while currentVal > 0
+    puts "#{currentVal} SECOND(S)!"
+    currentVal -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
