36785ccc9d31c499315bc458dea24c94f2910d6e
Nkenge Simms <simmsn@yahoo.com>
2020-07-22 00:20:53 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
+def countdown(n)
+  n = 11
+  while n >= 2
+  n -= 1
+ puts "#{n} SECOND(S)!"
+end
+return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(n)
+  sleep 5
 end
