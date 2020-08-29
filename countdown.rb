f28f2e4019450c729194313fcb1c7496ade2182b
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-08-29 22:00:39 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "Happy New Year!".upcase
+end
 
-def countdown
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+  "Happy New Year!".upcase
 end
