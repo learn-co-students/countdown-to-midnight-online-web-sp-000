5949aa3150b21a6af6f211744f0211eeec95ecfc
Robert Clarke <r.jason.clarke@gmail.com>
2020-09-23 00:16:31 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+
+def countdown(countdown_to_midnight)
+  countdown = 10
+
+  while countdown > 0
+    sleep(1)
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(countdown_to_midnight)
+  countdown = 10
+
+  while countdown > 0
+    sleep(1)
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
