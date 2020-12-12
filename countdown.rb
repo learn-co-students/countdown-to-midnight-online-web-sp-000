fd0d31ddda30de3e6b2139b39a4ad84af67d50e8
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-12 23:53:49 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(times)
+  while (times > 0)
+  puts "#{times} SECOND(S)!"
+    times-=1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+#write your code here
+
+def countdown_with_sleep(times)
+  while (times > 0)
+  puts "#{times} SECOND(S)!"
+    times-=1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
+
