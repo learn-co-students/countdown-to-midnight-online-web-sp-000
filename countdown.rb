2e56ea8683f4b96572a550d63f13b9b8cc1d1151
Shami Tomita <shamitomita@gmail.com>
2020-12-30 00:22:09 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
-
-def countdown
+x = 12
+def countdown(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+  x -= 1
+  if x == 0 
+    return "HAPPY NEW YEAR!"
+    end
+  end
 end
+
+y = 0 
+
+def countdown_with_sleep(y)
+  sleep(5)
+  while y < 5 
+  puts "#{x} sheep"
+end
+end 
