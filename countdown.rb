b041cca2bae694b1f664162f924f0d5ebd9769ba
Elliott Vargo <vargoelliott@gmail.com>
2021-01-17 00:21:53 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+x = 10
+def countdown(x)
+  while x > 0
+    puts "#{x} SECOND(S)!\n"
+    x -= 1
+end
+return "HAPPY NEW YEAR!"
+end
+x = 10
+def countdown_with_sleep(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep 1
 
-def countdown
+  end
+  return "HAPPY NEW YEAR!"
 end
