2de64d7596efc5d111dab7c535d55099433e0ce1
Christopher Black <christopher.black.2209@googlemail.com>
2021-03-30 18:00:51 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  while integer >= 1 
+  puts "#{integer} SECOND(S)!"
+  integer -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_to_sleep(integer)
+  while integer.seconds >= 1 
+  puts "#{integer} SECOND(S)"
+  integer -= 1 
+  end
 end
