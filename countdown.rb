1e14b51545ec9187b69cda36c2ef93e4d412adb3
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-10 18:35:38 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
-
-def countdown
+number = ()
+def countdown(number)
+  while number >= 1
+  puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(int)
+  sleep(int)
 end
