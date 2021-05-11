cc43bfe0d98e5c170c6b095872a10744b8d925a5
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-11 01:33:00 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  while integer == 12
+    puts "HAPPY NEW YEAR!"
+    integer -= 1
+  end
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+  end
+  while integer == 0
+    puts "HAPPY NEW YEAR!"
+  end
 end
+      #sleep(num_secs)
