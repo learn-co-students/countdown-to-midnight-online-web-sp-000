e2b86a443cac6c04f994a3f9e1ab9f0c055e2c7a
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-05-25 01:40:19 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,8 @@
 #write your code here
-countdown = 10
 
 def countdown(countdown)
   while countdown > 10
   countdown -= 1
   puts "#{countdown} SECOND (S)"
 end
-
-puts "#{countdown} SECOND (S)"
-
 end
