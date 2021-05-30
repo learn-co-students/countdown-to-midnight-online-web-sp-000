e3ce7f784ed89060ca3bed103e4a41ba1776e626
DJ Taylor <dtaylor88@gmail.com>
2021-05-30 05:59:44 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  x = integer
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(integer)
+  sleep integer
 end
